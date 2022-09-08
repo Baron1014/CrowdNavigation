65066e91a55238427a92c415226a63298d4afc88
Baron <04170133@gm.scu.edu.tw>
2022-09-08 15:52:44 +0800

    add check point in LinearEquation method

--- crowd_sim/envs/utils/utils.py
+++ crowd_sim/envs/utils/utils.py
@@ -48,3 +48,38 @@ def point_to_clostest(x1, y1, x2, y2, x3, y3):
     y = y1 + u * py
 
     return [x, y]
+
+
+def getCloestEdgeDist(x1, y1, x2, y2, robot_width, robot_length):
+    if abs(x1-x2) > robot_width and abs(y1-y2) > robot_length:
+        # right
+        if x1 > 0:
+            # if I else IV
+            min_dist = ((x1-robot_width)**2 + (y1-robot_length)**2)**0.5 if y1 > 0 else ((x1-robot_width)**2 + (y1-(-robot_length))**2)**0.5
+        # left
+        else:
+            robot_width = -robot_width
+            # if II else III
+            min_dist = ((x1-robot_width)**2 + (y1-robot_length)**2)**0.5 if y1 > 0 else ((x1-robot_width)**2 + (y1-(-robot_length))**2)**0.5
+    # right or left side
+    elif abs(x1-x2) > robot_width and abs(y1-y2) < robot_length:
+        min_dist = x1-x2-robot_width if x1 > 0 else abs(x1-x2+robot_width)
+    # top or bottom side
+    elif abs(x1-x2) < robot_width and abs(y1-y2) > robot_length:
+        min_dist = y1-y2-robot_length if y1 > 0 else abs(y1-y2+robot_length)
+    else:
+        min_dist = 0
+
+    return min_dist
+
+# ax + by + c = 0
+def checkinLinearEquation(x1, y1, x2, y2, x3, y3):
+    sign = 1
+    a = y2 - y1
+    if a < 0:
+        sign=-1
+        a = sign*a
+    b = sign*(x1-x2)
+    c = sign*(y1*x2-x1*y2)
+
+    return True if a*x3 + b*y3 + c == 0 else False
